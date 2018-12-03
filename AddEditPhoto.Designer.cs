1be770077c2d0b087a2d0d391b5b5cfbe7d7ddbd
Gabriel-Lachapelle <gabriellachapelle@live.ca>
2018-12-03 16:30:46 -0500

    Modification Carousel Settings

--- AddEditPhoto.Designer.cs
+++ AddEditPhoto.Designer.cs
@@ -236,6 +236,7 @@
             this.Controls.Add(this.GB_Info);
             this.Controls.Add(this.ImageBox);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(5);
             this.MinimumSize = new System.Drawing.Size(418, 652);
             this.Name = "AddEditPhoto";
--- CarouselSettings.Designer.cs
+++ CarouselSettings.Designer.cs
@@ -32,15 +32,20 @@
             this.BTN_Reset = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.TrackBarVitesse = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.TrackBarVitesse)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // CB_DefilementAleatoire
             // 
             this.CB_DefilementAleatoire.AutoSize = true;
-            this.CB_DefilementAleatoire.Location = new System.Drawing.Point(13, 13);
+            this.CB_DefilementAleatoire.Location = new System.Drawing.Point(9, 29);
+            this.CB_DefilementAleatoire.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.CB_DefilementAleatoire.Name = "CB_DefilementAleatoire";
-            this.CB_DefilementAleatoire.Size = new System.Drawing.Size(119, 17);
+            this.CB_DefilementAleatoire.Size = new System.Drawing.Size(170, 24);
             this.CB_DefilementAleatoire.TabIndex = 0;
             this.CB_DefilementAleatoire.Text = "Défilement aléatoire";
             this.CB_DefilementAleatoire.UseVisualStyleBackColor = true;
@@ -48,9 +53,10 @@
             // 
             // BTN_Reset
             // 
-            this.BTN_Reset.Location = new System.Drawing.Point(116, 206);
+            this.BTN_Reset.Location = new System.Drawing.Point(99, 250);
+            this.BTN_Reset.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BTN_Reset.Name = "BTN_Reset";
-            this.BTN_Reset.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Reset.Size = new System.Drawing.Size(100, 28);
             this.BTN_Reset.TabIndex = 1;
             this.BTN_Reset.Text = "Réinitialiser";
             this.BTN_Reset.UseVisualStyleBackColor = true;
@@ -59,38 +65,79 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Location = new System.Drawing.Point(9, 97);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.Size = new System.Drawing.Size(162, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Vitesse de défilement";
             // 
             // TrackBarVitesse
             // 
-            this.TrackBarVitesse.Location = new System.Drawing.Point(15, 62);
-            this.TrackBarVitesse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TrackBarVitesse.Location = new System.Drawing.Point(6, 138);
             this.TrackBarVitesse.Minimum = 1;
             this.TrackBarVitesse.Name = "TrackBarVitesse";
-            this.TrackBarVitesse.Size = new System.Drawing.Size(145, 45);
+            this.TrackBarVitesse.Size = new System.Drawing.Size(173, 45);
             this.TrackBarVitesse.TabIndex = 3;
             this.TrackBarVitesse.Value = 1;
             this.TrackBarVitesse.ValueChanged += new System.EventHandler(this.TrackBarVitesse_ValueChanged);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 188);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rapide";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 188);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lent";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CB_DefilementAleatoire);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.TrackBarVitesse);
+            this.groupBox1.Location = new System.Drawing.Point(13, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(186, 226);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
             // CarouselSettings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(199, 232);
-            this.Controls.Add(this.TrackBarVitesse);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(212, 288);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.BTN_Reset);
-            this.Controls.Add(this.CB_DefilementAleatoire);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "CarouselSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CarouselSettings";
             this.Load += new System.EventHandler(this.CarouselSettings_Load);
             ((System.ComponentModel.ISupportInitialize)(this.TrackBarVitesse)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -100,5 +147,8 @@
         private System.Windows.Forms.Button BTN_Reset;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TrackBar TrackBarVitesse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
